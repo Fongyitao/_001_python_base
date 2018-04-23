3138294a738c52793682a36b47b58b0fe7346bd0
Fong <Fongyitao>
2018-04-23 22:00:58 +0800

    1
    1

--- _011_递归和匿名函数/_003_lambda匿名函数应用2.py
+++ _011_递归和匿名函数/_003_lambda匿名函数应用2.py
@@ -1,7 +1,7 @@
 def test(a,b,func):
     return func(a,b)
 
-func=input("请输入lambda表达式：（例如lambda x,y:x+y）")
+func=input("请输入lambda表达式（例如lambda x,y:x+y）>>")
 func=eval(func)# 把字符串转换成表达式
 
 print(test(22,33,func))
