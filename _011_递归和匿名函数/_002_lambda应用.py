6accd65336d6a50e9c07a8f4e9ec8c520a296953
Fong <Fongyitao>
2018-04-23 22:13:43 +0800

    1
    1

--- _011_递归和匿名函数/_002_lambda应用.py
+++ _011_递归和匿名函数/_002_lambda应用.py
@@ -3,9 +3,11 @@ def test(a,b,func):
 
 print(test(22,33,lambda x,y:x+y))# 55
 
+#----------------------------------
+
 stus=[{"name":"Ryan","age":23},{"name":"Jack","age":18}]
 
-#匿名函数的作用：返回了列表中字典key为name的值，再有sort对其进行排序
+#匿名函数的作用：返回了列表中字典key为name的值，再用sort对其进行排序
 stus.sort(key=lambda x:x["name"],reverse=True) #采用降序排序，默认reverse=False是升序排序
 
 print(stus)
